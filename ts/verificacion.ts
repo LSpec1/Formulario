8d7cd40caf3576bb44812d27fd98780d112d31d4
Elian <toroelian4@gmail.com>
2021-09-29 20:13:17 -0300

    verificar-check1

--- ts/verificacion.ts
+++ ts/verificacion.ts
@@ -3,58 +3,59 @@ import jquery=require('jquery');
 const $:JQueryStatic=jquery;
 
 //verificar checkbox
-function validarCheckBox() {
-  function validarCheckBox() {
-    var casilla1:any = document.getElementById('check1');
-    var casilla2:any = document.getElementById('check2');
-    var casilla3:any = document.getElementById('check3');
-    var casilla4:any = document.getElementById('check4');
-    var casilla5:any = document.getElementById('check5');
-    var casilla6:any = document.getElementById('check6');
-    var casilla7:any = document.getElementById('check7');
-    var casilla8:any = document.getElementById('check8');
-    var casilla9:any = document.getElementById('check9');
-    var casilla10:any = document.getElementById('check10');
-    var casilla11:any = document.getElementById('check11');
-    var casilla12:any = document.getElementById('check12');
-    var alMenos_uno = false;
-    //verificacion
-    if (casilla1.checked) {
-        alMenos_uno = true;
-    } else if (casilla2.checked) {
-        alMenos_uno = true;
-    } else if (casilla3.checked) {
-        alMenos_uno = true;
-    } else if (casilla4.checked) {
-        alMenos_uno = true;
-    } else if (casilla5.checked) {
-        alMenos_uno = true;
-    } else if (casilla6.checked) {
-        alMenos_uno = true;
-    } else if (casilla7.checked) {
-        alMenos_uno = true;
-    } else if (casilla8.checked) {
-        alMenos_uno = true;
-    } else if (casilla9.checked) {
-        alMenos_uno = true;
-    } else if (casilla10.checked) {
-        alMenos_uno = true;
-    } else if (casilla11.checked) {
-        alMenos_uno = true;
-    } else if (casilla12.checked) {
-        alMenos_uno = true;
-    } else {
-        alMenos_uno = false;
-    }
-    if (alMenos_uno) {
-        const mensaje:any = document.getElementById('.error1');
-        mensaje.classList.add('activo');
-    }
-    else {
-      const mensaje:any = document.querySelector('.error1');
-      mensaje.classList.add('desactivado');
-    }
+function validarCheckBox1() {
+  var casilla1:any = document.getElementById('check1');
+  var casilla2:any = document.getElementById('check2');
+  var casilla3:any = document.getElementById('check3');
+  var casilla4:any = document.getElementById('check4');
+  var casilla5:any = document.getElementById('check5');
+  var casilla6:any = document.getElementById('check6');
+  var casilla7:any = document.getElementById('check7');
+  var casilla8:any = document.getElementById('check8');
+  var casilla9:any = document.getElementById('check9');
+  var casilla10:any = document.getElementById('check10');
+  var casilla11:any = document.getElementById('check11');
+  var casilla12:any = document.getElementById('check12');
+  var alMenos_uno = false;
+  //verificacion
+  if (casilla1.checked) {
+      alMenos_uno = true;
+  } else if (casilla2.checked) {
+      alMenos_uno = true;
+  } else if (casilla3.checked) {
+      alMenos_uno = true;
+  } else if (casilla4.checked) {
+      alMenos_uno = true;
+  } else if (casilla5.checked) {
+      alMenos_uno = true;
+  } else if (casilla6.checked) {
+      alMenos_uno = true;
+  } else if (casilla7.checked) {
+      alMenos_uno = true;
+  } else if (casilla8.checked) {
+      alMenos_uno = true;
+  } else if (casilla9.checked) {
+      alMenos_uno = true;
+  } else if (casilla10.checked) {
+      alMenos_uno = true;
+  } else if (casilla11.checked) {
+      alMenos_uno = true;
+  } else if (casilla12.checked) {
+      alMenos_uno = true;
+  } else {
+      alMenos_uno = false;
   }
+  if (alMenos_uno) {
+      const mensaje:any = document.getElementById('.error1');
+      mensaje.classList.add('activo');
+      return true;
+  }
+  else {
+    const mensaje:any = document.querySelector('.error1');
+    mensaje.classList.add('desactivado');
+    return false;
+  }
+
 }
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
@@ -69,7 +70,7 @@ function validarCheckBox() {
     .forEach(function (form) {
       form.addEventListener('submit', function (event:any) {
         // Checklist
-        validarCheckBox();
+        validarCheckBox1();
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
